359187122012b37d8fe7baec22d5a47519f7d1f8
Codeliner <codeliner96@gmail.com>
2021-12-11 22:57:43 +0900

    Code refactoring.

--- src/navigators/LoggedInNavigator.js
+++ src/navigators/LoggedInNavigator.js
@@ -1,17 +1,14 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabNavigators from "./BottomTabNavigators";
 import StackNavigators from "./StackNavigators";
-import { colors, getIsDark } from "../utils/themes/colors";
-import { ThemeContext } from "styled-components/native";
-import ProfileScreen from "../screens/settings/profiles/ProfileScreen";
 import useMe from "../hooks/useMe";
-import states from "../apollo/states";
 
 const Nav = createNativeStackNavigator();
 
 const LoggedInNavigator = () => {
   useMe();
+
   return (
     <Nav.Navigator
       screenOptions={{
